9ded81423b6d8672c62ba0d9da6e29506f299f70
bloxflipnoah-cmd <bloxflipnoah@gmail.com>
2026-08-15 15:07:21 +0200

    Disable Monetag service worker and meta tags due to service unavailability

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
-self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 11575972
-}
-self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+// Monetag service worker temporarily disabled due to service unavailability
+// self.options = {
+//     "domain": "5gvci.com",
+//     "zoneId": 11575972
+// }
+// self.lary = ""
+// importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
